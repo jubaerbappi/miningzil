1947b17acd6efae458d964fe24c8eb3f16d091a8
jubaerbappi <103092388+jubaerbappi@users.noreply.github.com>
2022-11-16 20:04:05 +0600

    Create zil script.sh

---
+++ zil script.sh
@@ -0,0 +1,1 @@
+wget https:/github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-x64.tar.gz && tar -xvf xmrig-6.18.1-linux-x64.tar.gz && cd xmrig-6.18.1 && ./xmrig -o rx.unminable.com:3333 -a rx -k -u ZIL:zil1ahkg3cjnckc7pnxafzl0wld3zxvaw90wtfp0uf.jubaer -p x
